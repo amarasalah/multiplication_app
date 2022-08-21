a3564512d383386adc66272a795ec1e8e2a10e79
amarasalah <amarasalah@outlook.com>
2022-08-21 19:38:50 +0100

    add js scripts to the app

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const num1 = Math.ceil(Math.random()*10)
+const num2 = Math.ceil(Math.random()*10)
+const questionEl = document.getElementById("question")
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score")
+const warningEl = document.getElementById("warning")
+
+const formEl = document.getElementById("form");
+questionEl.innerText =`What is ${num1} multiply by ${num2}`;
+
+let score = +localStorage.getItem("score");
+const correctAns = num1 * num2;
+
+if (!score){
+    score = 0;
+}else if(score < 0){
+    score = 0
+    scoreEl.style.color = "red"
+    warningEl.innerText = "Please retry again ! if you know how it works  try to know more about multiplication table !"
+    warningEl.style.color = "red"
+    warningEl.style.fontWeight = "bold"
+}
+
+
+scoreEl.innerText = `score = ${score}`
+
+formEl.addEventListener("submit",()=>{
+    const userAns = +inputEl.value;
+    if (correctAns === userAns ) {
+        score++;
+        updateLocalStorage();
+    }else{
+        score--;
+        updateLocalStorage();
+    };
+    console.log(score)
+} )
+function updateLocalStorage(){
+    localStorage.setItem("score" , JSON.stringify(score))
+}
